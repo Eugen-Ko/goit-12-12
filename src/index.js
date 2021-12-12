a8f42c702a70fa10517d792f0b3153b20cf0f01b
Eugen Kovalchuk <kovalchuk.eak@gmail.com>
2021-12-12 15:28:30 +0200

    11

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './sass/main.scss';
+import './js/getWeatherByCoords';
+import {getWeatherByCoords} from './js/getWeatherByCoords'
+
+let long = 0;
+let lat = 0;
+
+
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(position => {
+      
+    console.log(position)
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+    
+    getWeatherByCoords(lat, long)
+     .then (data => console.log(data));
+
+    console.log(`lat ${lat}`, `long ${long}`);
+
+  })
+}
+
